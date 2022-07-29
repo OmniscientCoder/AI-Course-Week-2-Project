9d478125cbb942c863ab0ee8a98d485a9d49d423
OmniscientCoder <109556740+OmniscientCoder@users.noreply.github.com>
2022-07-29 18:14:46 -0400

    nothing to see here

--- Week 1/Day_2_Functions.py
+++
@@ -1,7 +0,0 @@
-def compute_square(x):
-    result=x**2
-    return result
-sq1=compute_square(2)
-sq2=compute_square(4)
-print("square 1 is", sq1)
-print("square 2 is", sq2)
--- Week 1/Day_4_Importing_Libraries.py
+++
@@ -1,8 +0,0 @@
-from Day_3_Classes import Student as st
-from Day_3_Classes import Course as co
-
-s1=st("HAllo2qewa", 213)
-s1.studentInfoFunc()
-
-c1=co("AIIIIIII", 123907129)
-c1.courseInfoFunc()
